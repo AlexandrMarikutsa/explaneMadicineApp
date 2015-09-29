565dad41614f228565d8b550d5806abca07811ce
AlexandrMarikutsa <a.marikutsa@ukr.net>
2015-09-29 18:03:54 +0300

    added pause, but not working

--- app/src/main/java/com/demo/develop/explanemadicineapp/MainActivity.java
+++ app/src/main/java/com/demo/develop/explanemadicineapp/MainActivity.java
@@ -8,12 +8,17 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateInterpolator;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.SearchView;
 
+import com.transitionseverywhere.ChangeBounds;
+import com.transitionseverywhere.Fade;
 import com.transitionseverywhere.Scene;
 import com.transitionseverywhere.TransitionManager;
+import com.transitionseverywhere.TransitionSet;
 
 public class MainActivity extends Activity {
 
@@ -26,10 +31,20 @@ public class MainActivity extends Activity {
         final Scene scene = Scene.getSceneForLayout(container,
                 R.layout.search_layout, this);
 
-        ImageView goButton = (ImageView) findViewById(R.id.browse);
+        SearchView goButton = (SearchView) findViewById(R.id.searchView);
+
         goButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                TransitionSet set = new TransitionSet();
+                set.addTransition(new Fade());
+                set.addTransition(new ChangeBounds());
+                // выполняться они будут одновременно
+                set.setOrdering(TransitionSet.ORDERING_TOGETHER);
+                // уставим свою длительность анимации
+                set.setDuration(500);
+                // и изменим Interpolator
+                set.setInterpolator(new AccelerateInterpolator());
                 TransitionManager.go(scene);
             }
         });
