3c6260ad9db5614f537557a1817f69818d157d4a
AlexandrMarikutsa <a.marikutsa@ukr.net>
2015-09-30 11:31:31 +0300

    added back button

--- app/src/main/java/com/demo/develop/explanemadicineapp/MainActivity.java
+++ app/src/main/java/com/demo/develop/explanemadicineapp/MainActivity.java
@@ -15,41 +15,62 @@ import android.widget.ImageView;
 import android.widget.SearchView;
 
 import com.transitionseverywhere.ChangeBounds;
+import com.transitionseverywhere.Explode;
 import com.transitionseverywhere.Fade;
 import com.transitionseverywhere.Scene;
+import com.transitionseverywhere.Slide;
 import com.transitionseverywhere.TransitionManager;
 import com.transitionseverywhere.TransitionSet;
 
 public class MainActivity extends Activity {
+    private ViewGroup container;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ViewGroup container = (ViewGroup) findViewById(R.id.container);
+        container = (ViewGroup) findViewById(R.id.container);
         final Scene scene = Scene.getSceneForLayout(container,
                 R.layout.search_layout, this);
 
-        SearchView goButton = (SearchView) findViewById(R.id.searchView);
+        final SearchView goButton = (SearchView) findViewById(R.id.searchView);
 
         goButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                TransitionSet set = new TransitionSet();
-                set.addTransition(new Fade());
-                set.addTransition(new ChangeBounds());
-                // выполняться они будут одновременно
-                set.setOrdering(TransitionSet.ORDERING_TOGETHER);
-                // уставим свою длительность анимации
-                set.setDuration(500);
-                // и изменим Interpolator
-                set.setInterpolator(new AccelerateInterpolator());
-                TransitionManager.go(scene);
-            }
+                TransitionManager.go(scene,getT());
+                findViewById(R.id.back_button).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        onBackPressed();
+                    }
+                });
+            };
+
         });
     }
 
+//    private static void toggleVisibility(View... views) {
+//        for (View view : views) {
+//            boolean isVisible = view.getVisibility() == View.VISIBLE;
+//            view.setVisibility(isVisible ? View.INVISIBLE : View.VISIBLE);
+//        }
+//    }
 
+    @Override
+    public void onBackPressed() {
+        TransitionManager.go(Scene.getSceneForLayout(container,
+                R.layout.activity_main, this), getT());
+    }
 
+    private TransitionSet getT() {
+        TransitionSet set = new TransitionSet();
+        set.addTransition(new Fade());
+        set.addTransition(new ChangeBounds());
+        set.setOrdering(TransitionSet.ORDERING_TOGETHER);
+        set.setDuration(1300);
+        set.setInterpolator(new AccelerateInterpolator());
+        return set;
+    }
  }
