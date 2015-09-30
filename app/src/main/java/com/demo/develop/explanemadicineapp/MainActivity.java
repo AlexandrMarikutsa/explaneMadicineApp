7a429b0fb9c9c02fc7f3ee519a60e51c1fc3d4b2
AlexandrMarikutsa <a.marikutsa@ukr.net>
2015-09-30 14:32:16 +0300

    changed animation

--- app/src/main/java/com/demo/develop/explanemadicineapp/MainActivity.java
+++ app/src/main/java/com/demo/develop/explanemadicineapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.demo.develop.explanemadicineapp;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -15,6 +16,7 @@ import android.widget.ImageView;
 import android.widget.SearchView;
 
 import com.transitionseverywhere.ChangeBounds;
+import com.transitionseverywhere.ChangeImageTransform;
 import com.transitionseverywhere.Explode;
 import com.transitionseverywhere.Fade;
 import com.transitionseverywhere.Scene;
@@ -36,12 +38,18 @@ public class MainActivity extends Activity {
     }
 
     private TransitionSet getT() {
+        ChangeBounds changeBounds = new ChangeBounds();
+        changeBounds.setDuration(1800);
+        Fade fadeOut = new Fade(Fade.OUT);
+        fadeOut.setDuration(1800);
+        Fade fadeIn = new Fade(Fade.IN);
+        fadeIn.setDuration(1800);
         TransitionSet set = new TransitionSet();
-        set.addTransition(new Fade());
-        set.addTransition(new ChangeBounds());
         set.setOrdering(TransitionSet.ORDERING_TOGETHER);
-        set.setDuration(800);
-        set.setInterpolator(new AccelerateInterpolator());
+        set
+                .addTransition(fadeOut)
+                .addTransition(changeBounds)
+                .addTransition(fadeIn);
         return set;
     }
 
